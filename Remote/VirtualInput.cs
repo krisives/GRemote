6ae9bc55e15fc756675f74a61836cdf66f686ac0
Kristopher Ives <kristopher.ives@gmail.com>
2014-04-05 19:09:20 -0700

    Removed input for now

--- Remote/VirtualInput.cs
+++ Remote/VirtualInput.cs
@@ -4,19 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
-using Interceptor;
 
 namespace GRemote
 {
     class VirtualInput
     {
         IntPtr targetWindowPtr;
-        Input input;
+        //Input input;
 
         public VirtualInput(IntPtr targetWindowPtr)
         {
-            this.input = new Input();
-            this.targetWindowPtr = targetWindowPtr;
+            //this.input = new Input();
+           // this.targetWindowPtr = targetWindowPtr;
         }
 
         public void TriggerKey()
